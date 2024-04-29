abded133e8c808dcf5a6dafdc625d168baf3776b
albertoCC05 <alberto.cst05@gmail.com>
2024-04-29 10:31:52 +0200

    acabado_movimiento

--- Assets/Scripts_movment/CameraPosition.cs
+++ Assets/Scripts_movment/CameraPosition.cs
@@ -6,11 +6,16 @@ using UnityEngine;
 public class CameraPosition : MonoBehaviour
 {
 
+    // variables speed rotation and reference of camera
+
     [SerializeField] private GameObject referenceCamera;
     [SerializeField] private float cameraRotationSpeed;
 
+    // input mouse variable
+
     private float mouseInput;
 
+    //update
 
     private void Update()
     {
@@ -19,12 +24,17 @@ public class CameraPosition : MonoBehaviour
 
 
     }
+
+    //Set position of camera
+
     private void CameraPositionReference()
     {
         transform.position = referenceCamera.transform.position;
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, referenceCamera.transform.rotation.eulerAngles.y, referenceCamera.transform.rotation.eulerAngles.z);
         
     }
+
+    // rotation of camera ( up and down )
    private void CameraRotationVertical()
     {
         mouseInput = Input.GetAxis("Mouse Y");
--- Assets/Scripts_movment/PlayerController.cs
+++ Assets/Scripts_movment/PlayerController.cs
@@ -6,12 +6,25 @@ public class PlayerController : MonoBehaviour
 {
     private const string GROUNDTAG = "Ground";
 
+    
+
+    // set variables movment
+     
     [SerializeField] private float playerMovmentSpeed;
     [SerializeField] private float playerRotationSpeed;
     [SerializeField] private float JumpForce;
     [SerializeField] private Rigidbody playerRigidBody;
-
     private bool isOnTheGround = false;
+
+    // set variables animator
+
+    [SerializeField] private Animator playerAnimator;
+    private bool isMovingForward = false;
+    private bool isMovingBack = false;
+    private bool isMovingLeft = false;
+    private bool isMovingRight = false;
+
+   //Set variables inputs
 
     private float verticalInputMove;
     private float horizontalInputMove;
@@ -20,20 +33,34 @@ public class PlayerController : MonoBehaviour
 
     private void Start()
     {
+        // lock the cursor in the middle of the screen
+
         Cursor.lockState = CursorLockMode.Locked;
     }
    
     private void FixedUpdate()
     {
-        PlayerMovment();
+        // player movement
+
+        PlayerMovement();
         
         
     }
     private void Update()
     {
+       // player jump and rotation
+        
         JumpPlayer();
         PlayerRotation();
+
+        // Set variables of animators
+
+        MovementAnimations();
+        SetAnimationVariables();
     }
+
+    // jump Player Logic and fall down
+
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag(GROUNDTAG))
@@ -43,22 +70,105 @@ public class PlayerController : MonoBehaviour
             playerRigidBody.constraints = playerRigidBody.constraints | RigidbodyConstraints.FreezeRotationY;
 
 
-            Debug.Log(isOnTheGround);
+            
+
+        }
+    }
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag(GROUNDTAG))
+        {
+            isOnTheGround = false;
+
+            playerRigidBody.constraints = playerRigidBody.constraints | RigidbodyConstraints.FreezeRotationY;
+
+
+            
 
         }
     }
 
-    private void PlayerMovment()
+    //Player movement Logic
+
+    private void PlayerMovement()
     {
         verticalInputMove = Input.GetAxis("Vertical");
         horizontalInputMove = Input.GetAxis("Horizontal");
-        
-        Vector3 movement = (horizontalInputMove * transform.right + verticalInputMove * transform.forward).normalized * (playerMovmentSpeed);
-        playerRigidBody.velocity = new Vector3(movement.x, playerRigidBody.velocity.y,movement.z);
+
+
+
+        Vector3 movement = Vector3.zero;
+       
+
+        if ( verticalInputMove >= 0.1f || verticalInputMove <= -0.1f)
+        {
+            movement = (verticalInputMove * transform.forward).normalized * (playerMovmentSpeed);
+          // movement = new Vector3(0, playerRigidBody.velocity.y, verticalInputMove * playerMovmentSpeed);
+        }
+        else if (horizontalInputMove >= 0.1f || horizontalInputMove <= -0.1f)
+        {
+             movement = (horizontalInputMove * transform.right).normalized * (playerMovmentSpeed);
+           // movement = new Vector3(horizontalInputMove * playerMovmentSpeed, playerRigidBody.velocity.y, 0);
+        }
+
+
+
+        //  playerRigidBody.velocity = movement;
+        playerRigidBody.velocity = new Vector3(movement.x, playerRigidBody.velocity.y, movement.z); ;
+
+
 
 
 
     }
+    private void MovementAnimations()
+    {
+        if (verticalInputMove >= 0.1f)
+        {
+            isMovingForward = true;
+
+            isMovingBack = false;
+            isMovingRight = false;
+            isMovingLeft = false;
+        }
+        else if ( verticalInputMove <= -0.1f)
+        {
+            isMovingBack = true;
+
+            isMovingForward = false;
+            isMovingRight = false;
+            isMovingLeft = false;
+        }
+        else if ( horizontalInputMove >= 0.1f)
+        {
+            isMovingRight = true;
+
+            isMovingForward = false;
+            isMovingBack = false;
+            isMovingLeft = false;
+
+        }
+        else if ( horizontalInputMove <= -0.1f)
+        {
+            isMovingLeft = true;
+            isMovingForward = false;
+            isMovingRight = false;
+            isMovingBack = false;
+
+
+
+        }
+        else
+        {
+            isMovingForward = false;
+            isMovingRight = false;
+            isMovingBack = false;
+            isMovingLeft = false;
+        }
+    }
+
+    //Player rotation logic ( rotation with mouse )
+
     private void PlayerRotation()
     {
         mouseInputRotate = Input.GetAxis("Mouse X");
@@ -66,16 +176,37 @@ public class PlayerController : MonoBehaviour
 
        
     }
+
+    // Jump player logic
+
     private void JumpPlayer()
     {
         if (Input.GetKeyDown(KeyCode.Space) && isOnTheGround == true)
         {
+            
+
             Debug.Log("jump");
             playerRigidBody.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
             isOnTheGround = false;
 
             Debug.Log(isOnTheGround);
+
+            // jump animation
+
+            playerAnimator.SetTrigger("JumpInput");
         }
+    }
+
+    // animations set
+
+    private void SetAnimationVariables()
+    {
+        playerAnimator.SetBool("MovingForward", isMovingForward);
+        playerAnimator.SetBool("MovingLeft", isMovingLeft);
+        playerAnimator.SetBool("MovingRight", isMovingRight);
+        playerAnimator.SetBool("MovingBack", isMovingBack);
+
+        playerAnimator.SetBool("IsOnTheGround", isOnTheGround);
     }
   
 
